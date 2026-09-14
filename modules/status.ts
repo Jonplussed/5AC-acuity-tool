bb797814536540925123cb19789624b7822f6521
Jonathan Childress <jon@childr.es>
2026-09-14 14:01:37 -0700

    status as numerical enum

--- modules/status.ts
+++ modules/status.ts
@@ -1,18 +1,23 @@
+const STATUS_MS_STR = "MS";
+const STATUS_IMC_STR = "IMC";
+
 export enum Status {
-  MS = "MS",
-  IMC = "IMC",
+  MS,
+  IMC,
 }
 
-export const fromString = function(s: string): Status {
-  let status = Status[s.toUpperCase() as keyof typeof Status];
-
-  if (status == null) {
-    throw new Error(`Cannot convert "${s}" into patient status.`);
+export const fromString = (s: string): Status => {
+  switch (s.toUpperCase()) {
+    case STATUS_MS_STR: return Status.MS;
+    case STATUS_IMC_STR: return Status.IMC;
   }
 
-  return status;
+  throw new Error(`Cannot convert "${s}" into patient status.`);
 }
 
-export const toString = function(s: Status): string {
-  return s.toString();
+export const toString = (s: Status): string => {
+  switch (s) {
+    case Status.MS: return STATUS_MS_STR;
+    case Status.IMC: return STATUS_IMC_STR;
+  }
 }
