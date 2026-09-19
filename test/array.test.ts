8373b88854b4fb4e9701deb6b921ffcf6f427a33
Jonathan Childress <jon@childr.es>
2026-09-19 11:01:31 -0700

    love an elegant loop

--- test/array.test.ts
+++ test/array.test.ts
@@ -2,20 +2,18 @@ import { beforeEach, describe, expect, it, toEqual } from 'vitest'
 import { scooch } from '../modules/array.ts'
 
 describe('scooch()', () => {
-  let a1: number[];
-  let a2: number[];
-
-  beforeEach(() => {
-    a1 = [1,2,3,4,5];
-  });
+  let arr: number[];
+  beforeEach(() => { arr = [1,2,3,4,5] });
 
   it('moves an element forward in an array', () => {
-    a2 = [1,3,4,2,5];
-    expect(scooch(a1,1,3)).toEqual(a2);
+    expect(scooch(arr,1,3)).toEqual([1,3,4,2,5]);
   });
 
   it('moves an element backward in an array', () => {
-    a2 = [1,4,2,3,5];
-    expect(scooch(a1,3,1)).toEqual(a2);
+    expect(scooch(arr,3,1)).toEqual([1,4,2,3,5]);
+  });
+
+  it('does nothing if "from" and "to" are equal', () => {
+    expect(scooch(arr,2,2)).toEqual([1,2,3,4,5]);
   });
 });
