f7f261382c57df5d9da6db2b5129a2fe8d9766e2
Jonathan Childress <jon@childr.es>
2026-09-20 08:41:04 -0700

    "fillInitial" step is redundant

--- modules/roster.ts
+++ modules/roster.ts
@@ -22,14 +22,21 @@ export const empty = (n: AssignmentCount): Roster => {
 }
 
 export const fill = (roster: Roster, patientList: P.Patient[]): Roster => {
-  // 1. Sort patients from lowest to highest acuity.
+  let assign: A.Assignment
+  let patient: P.Patient;
+  let indexCurr: number;
+  let indexNew: number;
+
   P.sortByAcuityAsc(patientList);
-  // 2. Give each assignment one patient to start. This results in an array of
-  // assignments sorted by acuity ascending.
-  fillInitial(roster, patientList);
-  // 3. Assign each patient to the first assignment that passes all
-  // constraints. Reorder assignments to remain sorted.
-  fillRemaining(roster, patientList);
+
+  while (patientList.length > 0) {
+    patient = patientList.pop();
+    indexCurr = findAssignIndex(patient, roster.assignments);
+    assign = A.insert(roster.assignments[indexCurr], patient);
+    indexNew = findAcuityAscIndex(assign.totalAcuity, roster.assignments);
+    scooch(roster.assignments, indexCurr, indexNew);
+  }
+
   return roster;
 }
 
@@ -52,30 +59,4 @@ const findAcuityAscIndex = (acuity: P.Acuity, assigns: A.Assignment[]): number =
   let i = assigns.findIndex((a) => a.totalAcuity > acuity);
   if (i < 0) { return assigns.length - 1; }
   return i - 1; // Should be safe since "i" should never equal 0.
-}
-
-const fillInitial = (roster: Roster, patientList: P.Patient[]): P.Patient[] => {
-  for (let i = roster.assignments.length - 1; i >= 0; i--) {
-    if (patientList.length <= 0) { break; }
-    A.insert(roster.assignments[i], patientList.pop());
-  }
-
-  return patientList;
-}
-
-const fillRemaining = (roster: Roster, patientList: P.Patient[]): P.Patient[]  => {
-  let assign: A.Assignment
-  let patient: P.Patient;
-  let indexCurr: number;
-  let indexNew: number;
-
-  while (patientList.length > 0) {
-    patient = patientList.pop();
-    indexCurr = findAssignIndex(patient, roster.assignments);
-    assign = A.insert(roster.assignments[indexCurr], patient);
-    indexNew = findAcuityAscIndex(assign.totalAcuity, roster.assignments);
-    scooch(roster.assignments, indexCurr, indexNew);
-  }
-
-  return patientList;
 }
