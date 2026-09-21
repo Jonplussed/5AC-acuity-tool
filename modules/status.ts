2b796bf4a49debd2a95c2c723c346bb06e6b4e7c
Jonathan Childress <jon@childr.es>
2026-09-21 04:09:58 -0700

    minor status edit

--- modules/status.ts
+++ modules/status.ts
@@ -1,11 +1,13 @@
 export class Status {
-  static fromString(s: string): Status {
-    switch (s.toUpperCase()) {
-      case this.MS.label:  return this.MS;
-      case this.IMC.label: return this.IMC;
-    }
+  static MS:  Status = new Status(1, "MS");
+  static IMC: Status = new Status(2, "IMC");
 
-    throw new Error(`Cannot convert "${s}" into patient status.`);
+  static all = [this.MS, this.IMC];
+
+  static fromString(label: string): Status {
+    let status = this.all.find((s) => s.label == label.toUpperCase());
+    if (status) { return status; }
+    throw new Error(`Cannot convert "${label}" into patient status.`);
   }
 
   static highest(s1: Status, s2: Status): Status {
@@ -15,9 +17,6 @@ export class Status {
       return s2;
     }
   }
-
-  static MS:  Status = new Status(0, "MS");
-  static IMC: Status = new Status(1, "IMC");
 
   readonly value: number;
   readonly label: string;
